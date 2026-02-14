b87ca50f38eee2d084e42ab8e59d87610d16a5ed
Faizal Refendi <faizal_refendi.pm.me>
2026-02-14 17:06:48 +0800

    chore: add login log

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const bot = createBot(config)
 
 // Example: start anti-AFK plugin after login
 bot.once('login', () => {
+  console.log('🔑 Bot logged in')
   if (bot.antiAfk?.start) bot.antiAfk.start()
   if (bot.tpaGuard?.start) bot.tpaGuard.start()
 })
