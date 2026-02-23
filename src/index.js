68bc347827e1ab9ac0bab35a9ca5c9f631f55585
Faizal Refendi <faizal_refendi.pm.me>
2026-02-24 02:28:22 +0800

    chore: apply logger

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 // src/index.js
 const createBot = require('./createBot')
 const { envCheckAndGetConfig } = require('./config/env')
+const logger = require('./utils/logger')
 
 const config = envCheckAndGetConfig()
 const bot = createBot(config)
 
 // Example: start anti-AFK plugin after spawn
 bot.once('login', () => {
-  console.log('🔑 Bot logged in')
+  logger.log('🔑 Bot logged in')
 
   bot.on('spawn', () => {
     if (bot.antiAfk?.start) bot.antiAfk.start()
